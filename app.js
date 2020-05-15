422aa4d7ebbf78175210b7913e379008cf13b14e
Isaac Bustamante <izaack.97@gmail.com>
2020-05-14 21:31:19 -0700

    Se conecto con mongodb y crea ya en automoviles

--- app.js
+++ app.js
@@ -1,31 +1,26 @@
-require('dotenv').config();
-require('./private/database');
-
 const routerModulos = require('./routes/modulos');
-const morgan = require('morgan');
-const path = require('path');
+const routerAuth = require('./routes/auth');
+const morgan = require('morgan')
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
 // Settings
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.set('port', process.env.PORT || 8000)
 
 // Middlewars
-app.use(express.static(path.join(__dirname, 'public'))); //Middleware que montara al servidor archivos estaticos
-
+// Carpeta donde se estaran los recursos estaticos
+app.use(express.static('public')); //Middleware que montara al servidor archivos estaticos
+app.set('public', __dirname + '/public');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // Nos permitira leer los parametros que mandan en una peticion post
 app.use(morgan('dev')); // Utiliza morgan en forma de desarrollo, muestra las peticiones en consola
 
-// Global Variables
-
-
 // Routes
 app.use(routerModulos);
+app.use(routerAuth);
 
 // Ejecutamos el server
 app.listen(app.get('port'), () => {
