442a0fe079c168ad538046cf02931ce64c44dfcf
Isaac Bustamante <izaack.97@gmail.com>
2020-05-15 16:14:58 -0700

    merge con moongose

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
-const router = require('./routes/modulos');
+require('dotenv').config();
+require('./private/database');
+const routerModulos = require('./routes/modulos');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
 const morgan = require('morgan')
 const bodyParser = require('body-parser');
 const express = require('express');
+
+// Initializations
 const app = express();
 
 // Settings
 app.set('view engine', 'pug');
 app.set('port', process.env.PORT || 8000)
-
-// Middlewars
-// Carpeta donde se estaran los recursos estaticos
 app.use(express.static('public')); //Middleware que montara al servidor archivos estaticos
 app.set('public', __dirname + '/public');
 
+// Middlewars
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // Nos permitira leer los parametros que mandan en una peticion post
 app.use(morgan('dev')); // Utiliza morgan en forma de desarrollo, muestra las peticiones en consola
+app.use(methodOverride('_method'));
+app.use(session({
+    secret: 'palabra-secreta-2020',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(flash()); /* Guarda el mensaje en el servidor*/
+
+
+// Variables Globales
+app.use((req, res, next) => {
+    // req.locals.success_msg = req.flash('success_msg');
+    next();
+});
 
 // Routes
-app.use(router);
+app.use(routerModulos);
 
 // Ejecutamos el server
 app.listen(app.get('port'), () => {
