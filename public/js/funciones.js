fd4d8b3232d0ee8f2a00774d9cd57f541f7dd91f
Isaac Bustamante <izaack.97@gmail.com>
2020-06-07 19:58:41 -0700

    Commit nlcs

--- public/js/funciones.js
+++ public/js/funciones.js
@@ -1,3 +1,17 @@
+// $(() => {
+//     $(document).ajaxStart(function(){
+//         $('#loading').removeClass('hidden');
+//     });
+
+//     $(document).ajaxStop(function(){
+//         $('#loading').hide();
+//     });
+
+//     setTimeout(() => {
+//         $('#loading').hide();
+//     }, 100);
+// });
+
 function softReload(){
     console.log('reloading');
     window.top.location.reload(true);
